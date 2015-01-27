37b1811b7dcc1937fc2ac0549c2e271571b04999
Julius Kimuli <jkimuli@gmail.com>
2015-01-27 08:36:50 +0300

    First Commit

---
+++ rango/urls.py
@@ -0,0 +1,12 @@
+__author__ = 'julius'
+
+from django.conf.urls import patterns,url
+from rango import views
+
+urlpatterns = patterns(' ',
+
+    url(r'^$',views.index,name='index'),
+    url(r'^about/$',views.about,name='about')
+
+
+    )
